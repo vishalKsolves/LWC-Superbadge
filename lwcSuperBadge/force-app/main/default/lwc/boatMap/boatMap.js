8ec4c3e4459f1c69a2baeba6c0f4384623879813
Vishal Sharma <vsunite3@gmail.com>
2022-11-07 08:51:31 +0530

    Challenge 7

---
+++ lwcSuperBadge/force-app/main/default/lwc/boatMap/boatMap.js
@@ -0,0 +1,68 @@
+import { LightningElement, wire, api } from 'lwc';
+import { MessageContext , subscribe, APPLICATION_SCOPE } from 'lightning/messageService';
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const LONGITUDE_FIELD ='Boat__c.Geolocation__Longitude__s';
+const LATITUDE_FIELD = 'Boat__c.Geolocation__Latitude__s';
+const BOAT_FIELDS = [LONGITUDE_FIELD, LATITUDE_FIELD];
+
+export default class BoatMap extends LightningElement {
+    _subscription = null;
+    @api boatId;
+
+    @api
+    get recordId() {
+        return this.boatId;
+    }
+
+    set recordId(value) {
+        this.setAttribute('boatId', value);
+        this.boatId = value;
+    }
+
+    error = undefined;
+    mapMarkers = [];
+
+    @wire(MessageContext)
+    messageContext;
+
+    @wire(getRecord, { recordId: "$recordId", fields: BOAT_FIELDS })
+    wiredRecord({ error, data }) {
+        if (data) {
+            this.error = undefined;
+            const longitude = data.fields.Geolocation__Longitude__s.value;
+            const latitude = data.fields.Geolocation__Latitude__s.value;
+            this.updateMap(longitude, latitude);
+        } else if (error) {
+            this.error = error;
+            this.boatId = undefined;
+            this.mapMarkers = [];
+        }
+    }
+
+    subscribeMC() {
+        this.subscription = subscribe(
+            this.messageContext,
+            BOATMC,
+            (message) =>  { this.boatId = message.recordId },
+            { scope: APPLICATION_SCOPE }
+        );
+    }
+
+    connectedCallback() {
+        if (this.subscription || this.recordId) {
+            return;
+        }
+
+        this.subscribeMC();
+    }
+
+    // Creates the map markers array with the current boat's location for the map.
+    updateMap(Longitude, Latitude) {}
+
+    // Getter method for displaying the map component, or a helper method.
+    get showMap() {
+        return this.mapMarkers.length > 0;
+    }
+}
