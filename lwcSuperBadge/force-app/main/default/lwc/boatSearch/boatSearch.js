e98adecf5395b81d495c704c98ee8c3792611130
Vishal Sharma <vishal.sharma@ksolves.com>
2022-11-21 11:37:34 +0530

    Challenge 10

--- lwcSuperBadge/force-app/main/default/lwc/boatSearch/boatSearch.js
+++ lwcSuperBadge/force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,23 +1,26 @@
-import { LightningElement } from "lwc";
-import { NavigationMixin } from 'lightning/navigation'; 
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
- export default class BoatSearch extends NavigationMixin(LightningElement) {
-    _isLoading = false;
-    
+export default class BoatSearch extends NavigationMixin(LightningElement) {
+    isLoading = false;
+
+    // Handles loading event
     handleLoading() {
-        this._isLoading = true;
+        this.isLoading = true;
     }
-    
+
     handleDoneLoading() {
-        this._isLoading = false;
+        this.isLoading = false;
     }
-    
+
     // Handles search boat event
     // This custom event comes from the form
     searchBoats(event) {
-        
+        let boatTypeId = event.detail.boatTypeId;
+        this.template.querySelector('c-boat-search-results').searchBoats(boatTypeId);
+        this.handleDoneLoading();
     }
-    
+
     createNewBoat() {
         this[NavigationMixin.Navigate]({
             type: 'standard__objectPage',
@@ -27,5 +30,4 @@ import { NavigationMixin } from 'lightning/navigation';
             }
         });
     }
-  }
-  
+}
